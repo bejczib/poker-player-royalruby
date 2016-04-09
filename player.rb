6b45e4fa8b47342267a822373d204b1fbb5c08ac
Maurer-Somogyi Akos <mrakosms@Maurer-Somogyis-MacBook-Pro.local>
2016-04-09 14:19:11 +0200

    3333333

--- player.rb
+++ player.rb
@@ -9,18 +9,41 @@ class Player
     STDERR.puts "bejottunk a bet_request-be"
     STDERR.puts game_state
     STDERR.puts "========================="
-    STDERR.puts game_state['players'][0]['hole_cards'][0]['rank']
-    STDERR.puts game_state['players'][0]['hole_cards'][1]['rank']
-    0
-    # our_rank = card_converter(game_state)[0] + card_converter(game_state)[1]
     
-    # if our_rank > 15
-    # 	puts "high"
-    # 	10000
-    # else
-    # 	puts "low"
-    # 	0
-    # end
+    if cards(game_state).uniq.count == 1
+      STDERR.puts "high"
+      10000
+    else
+      STDERR.puts "low"
+      0
+    end
+  end
+
+  def cards(game_state)
+    if game_state['players'][0]['hole_cards'][0]['rank'] == "J"
+      card1 = 11
+    elsif game_state['players'][0]['hole_cards'][0]['rank'] == "Q"
+      card1 = 12
+    elsif game_state['players'][0]['hole_cards'][0]['rank'] == "K"
+      card1 = 13
+    elsif game_state['players'][0]['hole_cards'][0]['rank'] == "A"
+      card1 = 14
+    else 
+      card1 = game_state['players'][0]['hole_cards'][0]['rank'].to_i
+    end
+
+    if game_state['players'][0]['hole_cards'][1]['rank'] == "J"
+      card2 = 11
+    elsif game_state['players'][0]['hole_cards'][1]['rank'] == "Q"
+      card2 = 12
+    elsif game_state['players'][0]['hole_cards'][1]['rank'] == "K"
+      card2 = 13
+    elsif game_state['players'][0]['hole_cards'][1]['rank'] == "A"
+      card2 = 14
+    else 
+      card2 = game_state['players'][0]['hole_cards'][1]['rank'].to_i
+    end
+    [card1, card2]
   end
 
   def showdown(game_state)
