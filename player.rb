0efaf34096268c25a12c1dbdea34ed6144ea7e91
Maurer-Somogyi Akos <mrakosms@Maurer-Somogyis-MacBook-Pro.local>
2016-04-09 11:55:38 +0200

    revert

--- player.rb
+++ player.rb
@@ -6,14 +6,16 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    our_rank = card_converter(game_state)[0] + card_converter(game_state)[1]
-    if our_rank > 15
-    	puts "high"
-    	10000
-    else
-    	puts "low"
-    	0
-    end
+    10000
+
+    # our_rank = card_converter(game_state)[0] + card_converter(game_state)[1]
+    # if our_rank > 15
+    # 	puts "high"
+    # 	10000
+    # else
+    # 	puts "low"
+    # 	1
+    # end
   end
 
   def showdown(game_state)
