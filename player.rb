aef1136e0790c771aaf480b935710d627039f7bc
Maurer-Somogyi Akos <mrakosms@Maurer-Somogyis-MacBook-Pro.local>
2016-04-09 16:40:30 +0200

    1

--- player.rb
+++ player.rb
@@ -13,6 +13,7 @@ class Player
     if preflop(game_state)
       STDERR.puts "PREFLOP"
       if (game_state['in_action'] == game_state['dealer']) && (game_state['pot'] == game_state['small_blind'] * 3)
+        STDERR.puts "DEALER"
         return 10000
       else
         rank = strtinghand_strength(game_state)
