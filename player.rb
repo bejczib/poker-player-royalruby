fa745c6143daf4a40096990f26049f98f4566f78
Maurer-Somogyi Akos <mrakosms@Maurer-Somogyis-MacBook-Pro.local>
2016-04-09 14:10:07 +0200

    222

--- player.rb
+++ player.rb
@@ -11,15 +11,15 @@ class Player
     STDERR.puts "========================="
     STDERR.puts game_state['players'][0]['hole_cards'][0]['rank']
 
-    # our_rank = card_converter(game_state)[0] + card_converter(game_state)[1]
-    0
-    # if our_rank > 15
-    # 	puts "high"
-    # 	10000
-    # else
-    # 	puts "low"
-    # 	0
-    # end
+    our_rank = card_converter(game_state)[0] + card_converter(game_state)[1]
+    
+    if our_rank > 15
+    	puts "high"
+    	10000
+    else
+    	puts "low"
+    	0
+    end
   end
 
   def showdown(game_state)
