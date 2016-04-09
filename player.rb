23c06554d355516aa8b2d79d0ee5fc9ca210048b
Maurer-Somogyi Akos <mrakosms@Maurer-Somogyis-MacBook-Pro.local>
2016-04-09 11:43:31 +0200

    minimum 15

--- player.rb
+++ player.rb
@@ -1,14 +1,22 @@
 require_relative 'log'
+require_relative 'services'
 
 class Player
 
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    10000
+    our_rank = card_converter(game_state)[0] + card_converter(game_state)[1]
+    if our_rank > 15
+    	10000
+    else
+    	0
+    end
   end
 
   def showdown(game_state)
 
   end
 end
+
+
